245efaaa237873b2e05fdfb21fcc274470b47dc3
GastonFlorenciano <gaston__florenciano@hotmail.com>
2024-08-06 11:47:34 -0300

    INSTRUCCIONES EN EL README Y FINALIZACION

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# crud-tasks-backend
+# CRUD de Tareas 
+
+### Pasos para ejecutar el servidor
+
+1. Posicionarse en el directotio correspondiente:
+```bash
+cd crud-tasks-backend
+```
+
+2. Instalar todas las dependencias:
+
+```bash
+npm install
+```
+
+3. Ejecutar el servidor
+
+```bash
+npm run dev
+```
+
+4. Probar el servidor
+
+#### [http://localhost:3000](http://localhost:3000)
+
+- Para cambiar el puerto ir a la linea 6 del archivo src/app.js
+
+```javascript
+const PORT = 3000;
+```
+
+### Endpoints:
+
+- Utilizar Postman o Thunder Client
+
+`POST /tasks`: Añadir una nueva tarea.
+
+`GET /tasks`: Obtener todas las tareas.
+
+`GET /task/:id`: Obtener una tarea específica por su ID.
+
+`PUT /task/:id`: Actualizar una tarea específica por su ID.
+
+`DELETE /task/:id`: Eliminar una tarea específica por su ID.
--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
+//Se importan los módulos correspondientes
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
-app = express()
-const PORT = process.env.PORT ||  3000;
+const app = express() // Crea una instancia de la aplicación Express
+const PORT = process.env.PORT ||  3000; // Define el puerto en el que la aplicación escuchará (3000)
 
-app.use(cors());
-app.use(morgan('dev'));
-app.use(express.json());
+//Middlewares
+app.use(cors()); // Para permitir que la aplicación acceda a recursos en un servidor ubicado en otro dominio
+app.use(morgan('dev')); // Para registrar solicitudes HTTP en la consola en modo 'dev'
+app.use(express.json()); // Para parsear cuerpos de solicitudes en formato JSON
 
-app.use(require('./routes/tasks.routes'));
+app.use(require('./routes/tasks.routes')); // Middleware para manejar las rutas definidas en el archivo 'tasks.routes'
 
+// Inicia el servidor y escucha en el puerto especificado
 app.listen(PORT, () => console.log(`ESCUCHANDO EN EL PUERTO ${PORT}`))
